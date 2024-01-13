f63b23f8d3e52fd0292043bec732416541e01f51
YJM <dbwoaud26@naver.com>
2024-01-14 03:08:46 +0900

    Feat.플레이어 이동 1차완료, 달리기 속도 변화 구현 완료(애니메이션은 미완)

--- UnderCooked/Assets/OverCooked/Scripts/PlayerController.cs
+++ UnderCooked/Assets/OverCooked/Scripts/PlayerController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    Rigidbody PlayerRigidbody;
     private float _speed = 7.0f;
+
+    bool Run = false;
     void Start()
     {
         Managers.Input.KeyAction -= OnKeyboard;
         Managers.Input.KeyAction += OnKeyboard;
+        PlayerRigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
@@ -32,42 +36,98 @@ public class PlayerController : MonoBehaviour
         //if (moveDirection != Vector3.zero)
         //{
 
-        //    Quaternion fromRotation = transform.rotation;
+        //    //Quaternion fromRotation = transform.rotation;
         //    Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
-        //    transform.rotation = Quaternion.Slerp(fromRotation, toRotation, 0.03f);
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 0.05f);
+        //    //transform.rotation = Quaternion.Slerp(fromRotation, toRotation, 0.03f);
+        //    //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 360f);
 
+        //    transform.position += moveDirection * _speed * Time.deltaTime;
 
         //    //transform.Translate(moveDirection * Time.deltaTime * _speed);
-        //    transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        //    // transform.Translate(Vector3.forward * Time.deltaTime * _speed);
         //    //transform.position += Vector3.forward * Time.deltaTime * _speed;
         //}
 
+
+
+        //if(Input.GetKey(KeyCode.W) && Input.GetKeyDown(KeyCode.A))
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.06f);
+        //    transform.position += (Vector3.forward * Time.deltaTime * _speed + Vector3.left * Time.deltaTime * _speed) * 0.2f; 
+        //}
+
+
+
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.06f);
+        //    transform.position += Vector3.forward.normalized * Time.deltaTime * _speed;
+
+        //}
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.06f);
+        //    transform.position += Vector3.left * Time.deltaTime * _speed;
+        //}
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.06f);
+        //    transform.position += Vector3.back * Time.deltaTime * _speed;
+        //}
+
+
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.06f);
+        //    transform.position += Vector3.right * Time.deltaTime * _speed;
+        //}
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _speed = 10f;
+        }
+        else _speed = 6f;
+        
+        
+        
+
+        Vector3 moveDirection = Vector3.zero;
+
         if (Input.GetKey(KeyCode.W))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.06f);
-            transform.position += Vector3.forward * Time.deltaTime * _speed;
-
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.06f);
-            transform.position += Vector3.back * Time.deltaTime * _speed;
+            moveDirection += Vector3.forward;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.06f);
-            transform.position += Vector3.left * Time.deltaTime * _speed;
+            moveDirection += Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDirection += Vector3.back;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.06f);
-            transform.position += Vector3.right * Time.deltaTime * _speed;
+            moveDirection += Vector3.right;
         }
 
-        
+        if (moveDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 0.06f);
+        }
+
+       
+        PlayerRigidbody.position += moveDirection.normalized * Time.deltaTime * _speed;
+       
 
     }
 }
