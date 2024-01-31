c304f7f2b56e765f022b64bf3942f6300c89c398
rkdalswl789 <rkdalswl789@gmail.com>
2024-01-31 18:08:42 +0900

    Feat: 접시 생성, 플레이어와 상호작용, 테이블에 배치 완료 / 접시 반납부분 구현해야함

--- UnderCooked/Assets/Scripts/Item/PlateReturn.cs
+++ UnderCooked/Assets/Scripts/Item/PlateReturn.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class PlateReturn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int PlateNumber;
+
+
+    float _spawnTime = 1.0f;
+    int _maxNumber = 2;
+    string _plateName = "Plate";
+    Transform _spawnPos;
+    Transform _playerSpawnPoint;
+    KeyCode triggerKey = KeyCode.Space;
+    bool canInteract = false;
+    
+    List<GameObject> PlateList = new List<GameObject>();
+
+
+    private void Start()
     {
-        
+        _spawnPos = this.transform.Find("SpawnPos");
+        StartCoroutine(SpawnPlate());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (canInteract && Input.GetKeyDown(triggerKey))
+        {
+            if(PlateNumber > 0)
+            {
+                Managers.Resource.Instantiate(_plateName, _playerSpawnPoint.position, Quaternion.identity, _playerSpawnPoint.transform);
+                Managers.Resource.Destroy(PlateList[PlateNumber - 1]);
+                PlateList.RemoveAt(PlateNumber - 1);
+                PlateNumber--;
+            }
+        }
     }
+
+    IEnumerator SpawnPlate()
+    {
+        PlateNumber++;
+
+        yield return new WaitForSeconds(_spawnTime);
+
+        GameObject plate = Managers.Resource.Instantiate(_plateName, _spawnPos.position + new Vector3(0, (PlateNumber-1) * 0.05f, 0), Quaternion.identity);
+        PlateList.Add(plate);
+
+        if (PlateNumber < _maxNumber)
+            StartCoroutine(SpawnPlate());
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canInteract = true;
+            _playerSpawnPoint = other.transform.Find("SpawnPos");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canInteract = false;
+        }
+    }
+
+
 }
