26c70d1647d9a8d7e2b5d3fcf4984eec4c0310e8
KMJ <rkdalswl789@gmail.com>
2024-02-06 12:35:59 +0900

    Feat: 주문서 UI prefab 생성식으로 수정중

--- UnderCooked/Assets/Scripts/UI/OrderUI.cs
+++ UnderCooked/Assets/Scripts/UI/OrderUI.cs
@@ -6,10 +6,6 @@ public class OrderUI : MonoBehaviour
 {
     // x : -342~342 , y : 9
 
-    public GameObject Fish_Order;
-    public GameObject Prawn_Order;
-
-    RectTransform _fishPos;
     RectTransform _prawnPos;
 
     float _speed = 3.0f;
@@ -19,26 +15,43 @@ public class OrderUI : MonoBehaviour
 
     List<GameObject> OrderList = new List<GameObject>();
 
+
+    public GameObject Fish_Order;
+    public GameObject Prawn_Order;
     public delegate void OrderCheck();
     public static event OrderCheck FoodOrderCheck;
 
-    
+
+    GameObject test_fish;
+    RectTransform _fishPos;
+
+
     void Start()
     {
-        OrderList.Add(Fish_Order);
-        OrderList.Add(Prawn_Order);
+        // 초반 위치 설정
+        //_fishPos = Fish_Order.GetComponent<RectTransform>();
+        //_prawnPos = Prawn_Order.GetComponent<RectTransform>();
 
-        _fishPos = Fish_Order.GetComponent<RectTransform>();
-        _prawnPos = Prawn_Order.GetComponent<RectTransform>();
+        //_fishPos.anchoredPosition = new Vector2(_maxLength - _distance * 1, 9);     // #1
+        //_prawnPos.anchoredPosition = new Vector2(_maxLength - _distance * 0, 9);    // #2
 
-        _fishPos.anchoredPosition = new Vector2(_maxLength - _distance * 1, 9);
-        _prawnPos.anchoredPosition = new Vector2(_maxLength - _distance * 0, 9);
+        // 리스트 추가
+        //OrderList.Add(Fish_Order);
+        //OrderList.Add(Prawn_Order);
 
+        // 반납되는 음식이 들어올 때 이벤트 구독
         PassingGate.FoodOrderCheck += OrderListChecking;
 
         // distance는 list의 index값에 따라 달라짐
-        StartCoroutine(FishOrderAnimation(-_maxLength));
-        StartCoroutine(PrawnOrderAnimation(-_maxLength + _distance));
+        //StartCoroutine(PrawnOrderAnimation(-_maxLength + _distance));
+
+
+
+        test_fish = Managers.Resource.Instantiate("Fish_Order", new Vector2(1920,970), null, this.transform);
+        OrderList.Add(test_fish);
+        _fishPos = test_fish.GetComponent<RectTransform>();
+        StartCoroutine(FishOrderAnimation(-342));
+
     }
 
     void OnDestroy()
@@ -49,13 +62,21 @@ public class OrderUI : MonoBehaviour
 
     IEnumerator FishOrderAnimation(float distance)
     {
-        while(_fishPos.anchoredPosition.x > distance)
+        //while(_fishPos.anchoredPosition.x > distance)
+        //{
+        //    _fishPos.anchoredPosition -= new Vector2(_speed, 0);
+        //    yield return null;
+        //}
+
+        //_fishPos.anchoredPosition = new Vector2(distance, _originHeight);
+
+        while (_fishPos.anchoredPosition.x > distance)
         {
             _fishPos.anchoredPosition -= new Vector2(_speed, 0);
             yield return null;
         }
 
-        _fishPos.anchoredPosition = new Vector2(distance, _originHeight);
+        _fishPos.anchoredPosition = new Vector2(distance, 9);
     }
 
     IEnumerator PrawnOrderAnimation(float distance)
