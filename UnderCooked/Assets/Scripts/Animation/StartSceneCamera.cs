e3c7eb0a542530e80fcf398c926909dd8d57852a
KMJ <rkdalswl789@gmail.com>
2024-02-15 00:03:32 +0900

    Feat: 시작 화면 구현 완료

---
+++ UnderCooked/Assets/Scripts/Animation/StartSceneCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneCamera : MonoBehaviour
+{
+    Vector3 finalPos = new Vector3(-1.57f, 4.17f, -9.05f);
+    static Vector3 finalAngle = new Vector3(0, 7.83f, 0);
+    Quaternion finalRotation = Quaternion.Euler(finalAngle);
+
+
+    public IEnumerator CameraAnimation()
+    {
+        while (transform.position.x > finalPos.x || transform.position.y > finalPos.y || transform.position.z > finalPos.z)
+        {
+            transform.position = Vector3.Lerp(transform.position, finalPos, 3f * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, finalRotation, 5f * Time.deltaTime);
+            
+            yield return null;
+        }
+    }
+}
---
+++ UnderCooked/Assets/Scripts/UI/StartSceneUI.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class StartSceneUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    VanShutter _vanShutter;
+    StartSceneCamera _startCamera;
+
+    GameObject _startButton;
+    GameObject _exitButton;
+    Text _startText;
+    Text _selectedText;
+    Text _originalText;
+
+    string _playScene = "[2]Minji";
+
+    private void Start()
+    {
+        _vanShutter = FindObjectOfType<VanShutter>();
+        _startCamera = FindObjectOfType<StartSceneCamera>();
+
+        _startButton = transform.Find("StartButton").gameObject;
+        _exitButton = transform.Find("ExitButton").gameObject;
+        _startText = transform.Find("StartText").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(_vanShutter.ShutterAnimation());
+            StartCoroutine(_startCamera.CameraAnimation());
+
+            _startButton.SetActive(true);
+            _exitButton.SetActive(true);
+            _startText.text = "";
+        }
+    }
+
+
+    public void OnClickNextScene()
+    {
+        SceneManager.LoadScene(_playScene);
+    }
+
+    public void OnClickExit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                    Application.Quit();
+        #endif
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        eventData.selectedObject.GetComponent<Text>().color = Color.white;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        eventData.selectedObject.GetComponent<Text>().color = new Color(40, 138, 217);
+
+    }
+}
