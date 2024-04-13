32a20bbf0d8d2fb8a1f7994bee6e7504551cad38
KMJ <rkdalswl789@gmail.com>
2024-04-14 00:28:57 +0900

    Style: 변수명 변경

--- UnderCooked/Assets/Scripts/Item/Overlap.cs
+++ UnderCooked/Assets/Scripts/Item/Overlap.cs
@@ -5,7 +5,7 @@ public class Overlap : MonoBehaviour
 {
     float _radius = 0.41f;
     float _maxDistance = 0.7f; // 정면으로 체크할 최대 거리
-    Collider _short_Obj;
+    Collider _collidedObject;
     GameObject _selectedObject;
     Color _originalColor;
 
@@ -16,33 +16,33 @@ public class Overlap : MonoBehaviour
 
     void Update()
     {
+        Debug.Log(_selectedObject);
+
         Vector3 playerPosition = transform.position;
         Vector3 playerForward = transform.forward;
         RaycastHit hit;
 
         if (Physics.SphereCast(playerPosition, _radius, playerForward, out hit, _maxDistance, Layermask))
         {
-            _short_Obj = hit.collider;
-            SelectGameObject = _short_Obj.gameObject;
+            _collidedObject = hit.collider;
+            SelectGameObject = _collidedObject.gameObject;
 
             RestoreObjectColor();
 
             // 충돌한 객체의 Renderer 가져오기
             MeshRenderer objRenderer = SelectGameObject.GetComponent<MeshRenderer>();
-
-            _selectedObject = SelectGameObject;
-            
-            _originalColor = objRenderer.material.color;
-
             objRenderer.material.SetColor("_EmissionColor", new Color(0.5f, 0.45f, 0.4f, 0f));
             objRenderer.material.EnableKeyword("_EMISSION");
+            _originalColor = objRenderer.material.color;
+
+            _selectedObject = SelectGameObject;
 
             ObjectSelectEnter.Invoke(SelectGameObject);
         }
 
         else
         {
-            _short_Obj = null; // 충돌한 콜라이더가 없으면 Short_Obj를 null로 설정
+            _collidedObject = null; 
             SelectGameObject = null;
 
             RestoreObjectColor();
