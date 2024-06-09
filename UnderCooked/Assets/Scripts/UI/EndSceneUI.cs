ab9f7a8ff3cae2ff75818e00d5e7fe59f85908a6
KMJ <rkdalswl789@gmail.com>
2024-06-09 23:54:30 +0900

    Fix: GameEndUI와 GameTimerUI 병합 /  Ending -> End로 씬 이름 변경

--- UnderCooked/Assets/Scripts/UI/EndSceneUI.cs
+++ UnderCooked/Assets/Scripts/UI/EndSceneUI.cs
@@ -6,58 +6,83 @@ using UnityEngine.SceneManagement;
 
 public class EndSceneUI : MonoBehaviour
 {
-    int _addingScore = 20;
-    int _minusScore = 10;
-    int _totalScore;
-    int _successOrder;
-    int _failOrder;
-    int[] _firstStageScore = new int[3]{ 20, 60, 240 };
-    
+    bool         _canSpace = false;
+    [Header("GameScore")]
+    int          _addingScore = 20;
+    int          _minusScore = 10;
+    int          _successScore;
+    int          _failScore;
+    int          _totalScore;
+    int[]        _firstStageScore = new int[3]{ 20, 60, 240 };
+    [Header("OrderCount")]
+    int          _successOrder;
+    int          _failOrder;
+    [Header("LoadScene")]
+    string       _startScene = "[1]Start";
+    string       _playScene = "[2]Game";
+    [Header("Pass, Fail")]
+    [SerializeField]
+    Text[]       _order;
+    [Header("Pass, Fail, Total")]
+    [SerializeField]
+    Text[]       _score;
+    [Header("Star1, Star2, Star3")]
+    [SerializeField]
+    GameObject[] _star;
 
 
-    string _startScene = "[1]Start";
-    string _playScene = "[2]Game";
-
-    bool _canSpace = false;
-
-
-    void Start()
+    /*
+     * PlayerPrefs에서 성공, 실패한 주문 수를 받아옴
+     * -> 성공, 실패, 합계 점수에 반영됨
+     * 게임 종료 사운드 재생
+     * 4초 이후에 "PlaySound" 함수를 실행함
+     * 게임 결과를 보여주는 함수 실행
+     */
+    void Awake()
     {
         _successOrder = PlayerPrefs.GetInt("Success");
         _failOrder = PlayerPrefs.GetInt("Fail");
-        
+
+        _successScore = _successOrder * _addingScore;
+        _failScore = _failOrder * _minusScore;
+        _totalScore = _successScore - _failScore;
+        if (_totalScore < 0)
+            _totalScore = 0;
+
         Managers.Sound.Clear();
         Managers.Sound.Play("AudioClip/LevelVictorySound", Define.Sound.Effect);
-        Invoke("PlaySound", 4f);
         
-        ChangeText();
-        TurnOnStar();
+        Invoke("PlaySound", 4f);
+        ShowGameResult();
     }
 
+
+    /*
+     * 조건이 충족되면 키 입력에 따라 다른 씬으로 넘어감
+     */
     private void Update()
     {
         if (_canSpace)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                LoadScene(_startScene);
+                PlayerPrefs.SetString("SceneName", _startScene);
+                PlayerPrefs.Save();
+                SceneManager.LoadScene("Loading");
             }
             else if (Input.GetKeyDown(KeyCode.LeftShift))
             {
-                LoadScene(_playScene);
+                PlayerPrefs.SetString("SceneName", _playScene);
+                PlayerPrefs.Save();
+                SceneManager.LoadScene("Loading");
             }
         }
     }
 
 
-    void LoadScene(string sceneName)
-    {
-        PlayerPrefs.SetString("SceneName", sceneName);
-        PlayerPrefs.Save();
-        SceneManager.LoadScene("Loading");
-    }
-
-
+    /*
+     * Bgm이 재생되는 함수
+     */
     void PlaySound()
     {
         Managers.Sound.Play("AudioClip/RoundResults", Define.Sound.Bgm);
@@ -65,50 +90,47 @@ public class EndSceneUI : MonoBehaviour
     }
 
 
-    void TurnOnStar()
+    /*
+     * 최종 결과를 보여주는 함수
+     * -> 최종 점수에 따라 별이 활성화되는 애니메이션이 재생됨
+     * -> 최종 결과표의 text UI를 변경함
+     */
+    void ShowGameResult()
     {
         if(_totalScore >= _firstStageScore[0])
         {
-            GameObject star = Define.FindDeepChild(transform, "Star1_Filled").gameObject;
-            StartCoroutine(ActivateStar(star, 0.5f, "AudioClip/RoundResults_Star_01"));
+            StartCoroutine(ActivateStar(_star[0], 0.5f, "AudioClip/RoundResults_Star_01"));
+
+            if (_totalScore >= _firstStageScore[1])
+            {
+                StartCoroutine(ActivateStar(_star[1], 1.5f, "AudioClip/RoundResults_Star_02"));
+
+                if (_totalScore >= _firstStageScore[2])
+                {
+                    StartCoroutine(ActivateStar(_star[2], 2.5f, "AudioClip/RoundResults_Star_03"));
+                }
+            }
         }
-        if(_totalScore >= _firstStageScore[1])
-        {
-            GameObject star = Define.FindDeepChild(transform, "Star2_Filled").gameObject;
-            StartCoroutine(ActivateStar(star,1.5f, "AudioClip/RoundResults_Star_02"));
-        }
-        if (_totalScore >= _firstStageScore[2])
-        {
-            GameObject star = Define.FindDeepChild(transform, "Star3_Filled").gameObject;
-            StartCoroutine(ActivateStar(star, 2.5f, "AudioClip/RoundResults_Star_03"));
-        }
+
+        _order[0].text = _successOrder.ToString();
+        _order[1].text = _failOrder.ToString();
+        
+        _score[0].text = _successScore.ToString();
+        _score[1].text = _failScore.ToString();
+        _score[2].text = _totalScore.ToString();
     }
 
 
+    /*
+     * Star를 활성화시키는 코루틴
+     * -> delay 시간 이후에 star를 나타내며 이펙트 효과음이 재생됨
+     */
     IEnumerator ActivateStar(GameObject star, float delay, string soundEffect)
     {
         yield return new WaitForSeconds(delay);
         
         star.SetActive(true);
         Managers.Sound.Play(soundEffect, Define.Sound.Effect);
-    }
-
-
-    void ChangeText()
-    {
-        Define.FindDeepChild(transform, "PassCount").GetComponent<Text>().text = _successOrder.ToString();
-        Define.FindDeepChild(transform, "FailCount").GetComponent<Text>().text = _failOrder.ToString();
-
-        int successScore = _successOrder * _addingScore;
-        int failScore = _failOrder * _minusScore;
-        _totalScore = successScore - failScore;
-
-        if (_totalScore < 0)
-            _totalScore = 0;
-
-        Define.FindDeepChild(transform, "PassScore").GetComponent<Text>().text = successScore.ToString();
-        Define.FindDeepChild(transform, "FailScore").GetComponent<Text>().text = failScore.ToString();
-        Define.FindDeepChild(transform, "TotalScore").GetComponent<Text>().text = _totalScore.ToString();
     }
 
 }
--- UnderCooked/Assets/Scripts/UI/GameTimerUI.cs
+++ UnderCooked/Assets/Scripts/UI/GameTimerUI.cs
@@ -1,28 +1,39 @@
-using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class GameTimerUI : MonoBehaviour
 {
-    Image _timerProgressBar;
-    Text _timeText;
-    float _timeLimit = 100f;
-    float _currentTime;
-    bool _canUpdate = true;
+    float       _timeLimit = 100f;
+    float       _currentTime;
+    bool        _canUpdate = true;
+    string      _endScene = "[3]End";
+    [SerializeField]
+    Image       _timerProgressBar;
+    [SerializeField]
+    Text        _timeText;
+    [SerializeField]
+    GameObject  _endImage;
+    
 
-
-    public static Action GameEnd;
-
-
-    void Start()
+    /*
+     * 현재 시간 초기화
+     */
+    void Awake()
     {
-        _timeText = Define.FindDeepChild(transform, "Time").GetComponent<Text>();
-        _timerProgressBar = Define.FindDeepChild(transform, "ProgressBar").GetComponent<Image>();
         _currentTime = _timeLimit;
+        _timeText.text = FormatTime(_currentTime);
     }
 
 
+    /*
+     * Time이 흐를 때만 현재 시간을 변화시킴
+     * -> 종료시 AppearEndingObject() 함수 호출
+     * -> FormatTime에 맞춰 _timeText 변화
+     * -> 시간에 맞춰 타이머바의 이미지 변화
+     */
     void Update()
     {
         if (_canUpdate)
@@ -32,12 +43,12 @@ public class GameTimerUI : MonoBehaviour
                 _currentTime -= Time.deltaTime;
             }
 
-            // 종료 조건
             if (_currentTime <= 0)
             {
                 _currentTime = 0;
-                GameEnd.Invoke();
                 _canUpdate = false;
+
+                AppearEndingObject();
             }
 
             _timeText.text = FormatTime(_currentTime);
@@ -46,11 +57,40 @@ public class GameTimerUI : MonoBehaviour
     }
 
 
+    /*
+     * 시간값을 00:00 포멧에 맞춰 바꿔주는 함수
+     */
     string FormatTime(float time)
     {
         int minutes = Mathf.FloorToInt(time / 60f);
         int seconds = Mathf.FloorToInt(time % 60f);
         return string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+
+    /*
+     * 게임 종료시 나오는 GameObject 함수
+     * -> bgm 종료 및 이펙트 효과음 재생
+     * -> 다음씬 로드
+     */
+    void AppearEndingObject()
+    {
+        _endImage.SetActive(true);
+        
+        Managers.Sound.Play("AudioClip/TimesUpSting", Define.Sound.Effect);
+        Managers.Sound.GetAudio(Define.Sound.Bgm).Stop();
+        
+        StartCoroutine(LoadNextScene());
+    }
+
+
+    /*
+     * 5초 뒤 다음 씬 로드
+     */
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene(_endScene);
     }
 
 }
